529ddf4a9377320d96cc2db488ccd6b36c639bc2
bamboozaa <chukadee@gmail.com>
2024-02-13 12:11:32 +0700

    pc commit

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -18,7 +18,7 @@
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="{{ route('home') }}">{{ __('Home') }}</a></li>
-                        <li class="breadcrumb-item"><a href="{{ url('students') }}">{{ __('Students') }}</a></li>
+                        {{-- <li class="breadcrumb-item"><a href="{{ url('students') }}">{{ __('Students') }}</a></li> --}}
                         <li class="breadcrumb-item active" aria-current="page">{{ __('Create Student') }}</li>
                     </ol>
                 </nav>
