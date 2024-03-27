d78014bd230be947914318e4493d1879923f1d3e
bamboozaa <chukadee@gmail.com>
2024-03-27 14:51:47 +0700

    PC Commit

--- resources/views/layouts/sidemenu.blade.php
+++ resources/views/layouts/sidemenu.blade.php
@@ -1,6 +1,6 @@
 <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0" style="background-color: #2e2f82;">
     <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 min-vh-100">
-        <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+        <ul class="nav flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
             <li>
                 <a href="{{ route('home') }}" class="nav-link px-0 align-middle text-white {{ request()->routeIs('home') ? 'active' : '' }}">
                     <i class="fs-5 bi-speedometer2"></i><span class="ms-2 d-none d-sm-inline">{{ __('Dashboard') }}</span>
@@ -16,22 +16,6 @@
                     <i class="bi bi-people fs-5"></i><span class="ms-2 d-none d-sm-inline">{{ __('ข้อมูลนักศึกษา') }}</span>
                 </a>
             </li>
-            {{-- <li>
-                <a href="#submenu3" data-bs-toggle="collapse" class="nav-link px-0 align-middle text-white">
-                    <i class="fs-5 bi-people"></i><span class="ms-2 d-none d-sm-inline">{{ __('ข้อมูลนักศึกษา') }}</span> <i
-                        class="bi bi-caret-down"></i>
-                </a>
-                <ul class="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
-                    <li class="w-100">
-                        <a href="#" class="nav-link px-0 text-white ms-2"><i class="bi bi-plus-lg fs-6"></i><span
-                                class="d-none d-sm-inline ms-2">{{ __('เพิ่มข้อมูลนักศึกษา') }}</span></a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link px-0 text-white ms-2"><i class="bi bi-display fs-6"></i><span
-                                class="d-none d-sm-inline ms-2">{{ __('แสดงรายชื่อนักศึกษา') }}</span></a>
-                    </li>
-                </ul>
-            </li>--}}
             <li>
                 <a href="#submenu4" data-bs-toggle="collapse" class="nav-link px-0 align-middle text-white">
                     <i class="bi bi-gear fs-5"></i><span class="ms-2 d-none d-sm-inline">{{ __('ตั้งค่าระบบ') }}</span> <i
