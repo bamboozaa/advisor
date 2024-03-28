11049f6def3c2710a1f5b19b0bb52e63c212b136
bamboozaa <chukadee@gmail.com>
2024-03-28 13:25:02 +0700

    PC Commit

--- resources/views/layouts/sidemenu.blade.php
+++ resources/views/layouts/sidemenu.blade.php
@@ -3,52 +3,52 @@
         <ul class="nav flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
             <li>
                 <a href="{{ route('home') }}" class="nav-link px-0 align-middle text-white {{ request()->routeIs('home') ? 'active' : '' }}">
-                    <i class="fs-5 bi-speedometer2"></i><span class="ms-2 d-none d-sm-inline">{{ __('Dashboard') }}</span>
+                    <i class="fs-4 bi-speedometer2"></i><span class="ms-2 d-none d-sm-inline">{{ __('Dashboard') }}</span>
                 </a>
             </li>
             <li>
                 <a href="{{ route('advisors.index') }}" class="nav-link px-0 align-middle text-white {{ request()->routeIs('advisors.*') ? 'active' : '' }}">
-                    <i class="bi bi-people-fill fs-5"></i><span class="ms-2 d-none d-sm-inline">{{ __('ข้อมูลอาจารย์ที่ปรึกษา') }}</span>
+                    <i class="bi bi-people-fill fs-4"></i><span class="ms-2 d-none d-sm-inline">{{ __('ข้อมูลอาจารย์ที่ปรึกษา') }}</span>
                 </a>
             </li>
             <li>
                 <a href="{{ route('students.index') }}" class="nav-link px-0 align-middle text-white {{ request()->routeIs('students.*') ? 'active' : '' }}">
-                    <i class="bi bi-people fs-5"></i><span class="ms-2 d-none d-sm-inline">{{ __('ข้อมูลนักศึกษา') }}</span>
+                    <i class="bi bi-people fs-4"></i><span class="ms-2 d-none d-sm-inline">{{ __('ข้อมูลนักศึกษา') }}</span>
                 </a>
             </li>
             <li>
                 <a href="#submenu4" data-bs-toggle="collapse" class="nav-link px-0 align-middle text-white">
-                    <i class="bi bi-gear fs-5"></i><span class="ms-2 d-none d-sm-inline">{{ __('ตั้งค่าระบบ') }}</span> <i
+                    <i class="bi bi-gear fs-4"></i><span class="ms-2 d-none d-sm-inline">{{ __('ตั้งค่าระบบ') }}</span> <i
                         class="bi bi-caret-down"></i>
                 </a>
                 <ul class="collapse nav flex-column ms-1 {{ request()->routeIs('academics.*') || request()->routeIs('qualifications.*') || request()->routeIs('departments.*') || request()->routeIs('faculties.*') || request()->routeIs('majors.*') ? 'show' : '' }}" id="submenu4" data-bs-parent="#menu">
                     <li class="w-100">
                         <a href="{{ route('academics.index') }}" class="nav-link px-0 text-white ms-2 {{ request()->routeIs('academics.*') ? 'active' : '' }}">
-                            <i class="bi bi-sliders fs-6"></i>
+                            <i class="bi bi-sliders fs-5"></i>
                             <span class="d-none d-sm-inline ms-2">{{ __('ตำแหน่งทางวิชาการ') }}</span>
                         </a>
                     </li>
                     <li>
                         <a href="{{ route('qualifications.index') }}" class="nav-link px-0 text-white ms-2 {{ request()->routeIs('qualifications.*') ? 'active' : '' }}">
-                            <i class="bi bi-sliders fs-6"></i>
+                            <i class="bi bi-sliders fs-5"></i>
                             <span class="d-none d-sm-inline ms-2">{{ __('วุฒิการศึกษา') }}</span>
                         </a>
                     </li>
                     <li>
                         <a href="{{ route('departments.index') }}" class="nav-link px-0 text-white ms-2 {{ request()->routeIs('departments.*') ? 'active' : '' }}">
-                            <i class="bi bi-sliders fs-6"></i>
+                            <i class="bi bi-sliders fs-5"></i>
                             <span class="d-none d-sm-inline ms-2">{{ __('หน่วยงาน') }}</span>
                         </a>
                     </li>
                     <li>
                         <a href="{{ route('faculties.index') }}" class="nav-link px-0 text-white ms-2 {{ request()->routeIs('faculties.*') ? 'active' : '' }}">
-                            <i class="bi bi-sliders fs-6"></i>
+                            <i class="bi bi-sliders fs-5"></i>
                             <span class="d-none d-sm-inline ms-2">{{ __('หลักสูตร/คณะ') }}</span>
                         </a>
                     </li>
                     <li>
                         <a href="{{ route('majors.index') }}" class="nav-link px-0 text-white ms-2 {{ request()->routeIs('majors.*') ? 'active' : '' }}">
-                            <i class="bi bi-sliders fs-6"></i>
+                            <i class="bi bi-sliders fs-5"></i>
                             <span class="d-none d-sm-inline ms-2">{{ __('สาขาวิชา/กลุ่มวิชา') }}</span>
                         </a>
                     </li>
