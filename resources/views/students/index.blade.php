331cff4351ed46bb44ec8084b36ba915499ad539
bamboozaa <chukadee@gmail.com>
2024-06-27 13:37:20 +0700

    PC Commit

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -27,6 +27,29 @@
             });
         @endif
 
+        let timerInterval;
+        Swal.fire({
+            title: "Please wait...!",
+            // html: "I will close in <b></b> milliseconds.",
+            timer: 2000,
+            timerProgressBar: true,
+            didOpen: () => {
+                Swal.showLoading();
+                const timer = Swal.getPopup().querySelector("b");
+                timerInterval = setInterval(() => {
+                    // timer.textContent = `${Swal.getTimerLeft()}`;
+                }, 100);
+            },
+            willClose: () => {
+                clearInterval(timerInterval);
+            }
+        }).then((result) => {
+            /* Read more about handling dismissals below */
+            if (result.dismiss === Swal.DismissReason.timer) {
+                console.log("I was closed by the timer");
+            }
+        });
+
         // DataTable.ext.errMode = 'throw';
         // new DataTable('#example');
     </script>
